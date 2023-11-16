11030e0dac045fcc7adc14f616954d3592aa3a26
chujianyu <yu_chn@outlook.com>
2023-11-16 00:51:39 -0500

    Added a color module to represent color

---
+++ lib/color.ml
@@ -0,0 +1,25 @@
+[@@@warning "-69-27-33-32"]
+open Vector
+(* module Color, with r g b values b/w 0. and 1.*)
+module Color = 
+  struct
+    type t = {
+      r : float;
+      g : float;
+      b : float;
+    }
+
+    let make (r: float) (g: float) (b: float) = { r = r; g = g; b = b }
+    let color_to_tuple col = (col.r, col.g, col.b)
+
+    let color_to_vector color = Vector3f.create ~x:color.r ~y:color.g ~z:color.b
+    let from_vector (v:Vector3f.t) = 
+      let (r, g, b) = Vector3f.to_tuple v in
+      make r g b 
+
+    let randColor () = {
+      r = Random.float 1.;
+      g = Random.float 1.;
+      b = Random.float 1.;
+    }
+  end
