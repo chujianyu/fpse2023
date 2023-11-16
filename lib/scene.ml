5154b6c5ec96652ed998a3029e01f372fb33f0c5
chujianyu <yu_chn@outlook.com>
2023-11-16 00:57:50 -0500

    updated scene to incorporate first class module list of shapes and lights

--- lib/scene.ml
+++ lib/scene.ml
@@ -1,13 +1,16 @@
+[@@@warning "-69-27-33"]
+
 open Ray
 open Vector
+
 open Camera
 open Light
 open Shape
-open Color
-module Scene : sig
+
+module Scene = struct
   type intersect_record_t = {intersection_time: float; position: Vector3f.t; normal: Vector3f.t; }
   type global_data_t = {camera:Camera.t; lights:(module L) list; shapes:(module S) list}
   type t
-  val get_color : Ray.t -> int -> int -> Vector3f.t
-  val ray_trace : t -> Color.t list list
+  let get_color _ _ _ =  Vector3f.empty ()
+  let ray_trace  _ = [[]]
 end
