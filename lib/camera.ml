ca578669633fa7f25e34e31f516cb22b0902530b
chujianyu <yu_chn@outlook.com>
2023-11-16 00:50:54 -0500

    updated camera signature

--- lib/camera.ml
+++ lib/camera.ml
@@ -3,6 +3,11 @@ open Vector
 open Ray
 
 module Camera = struct
+
+  type t = {heightAngle : float; pos : Vector3f.t; up : Vector3f.t; forward : Vector3f.t}
+
+  let create ~heightAngle ~pos ~up ~forward = {heightAngle; pos; up; forward}
+
   let getRay ~i ~j ~width ~height = 
     let orig = Vector3f.empty () in
     let dir = Vector3f.empty () in
--- lib/camera.mli
+++ lib/camera.mli
@@ -3,6 +3,9 @@ open Vector
 open Ray
 
 module Camera : sig
+  type t 
+
+  val create : heightAngle:float -> pos:Vector3f.t -> up:Vector3f.t -> forward:Vector3f.t -> t
   (* Given indices i and j, and the width and height of the image, return the ray shot through that pixel *)
   val getRay : i:int -> j:int -> width:int -> height:int -> Ray.t
 end
