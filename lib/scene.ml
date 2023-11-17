2e226f32990990297805c2ca17ef964dc417c7f2
chujianyu <yu_chn@outlook.com>
2023-11-16 21:01:09 -0500

    fixed signatures and added logic to iterate over pixels

--- lib/scene.ml
+++ lib/scene.ml
@@ -6,11 +6,24 @@ open Vector
 open Camera
 open Light
 open Shape
+open Core
+open Color
+
+module Intersection_record = struct
+  type t = {intersection_time: float; position: Vector3f.t; normal: Vector3f.t; }
+end
 
 module Scene = struct
-  type intersect_record_t = {intersection_time: float; position: Vector3f.t; normal: Vector3f.t; }
-  type global_data_t = {camera:Camera.t; lights:(module L) list; shapes:(module S) list}
-  type t
-  let get_color _ _ _ =  Vector3f.empty ()
-  let ray_trace  _ = [[]]
+  type t = {camera:Camera.t; lights:(module L) list; shapes:(module S) list}
+  let get_color _ ~i ~j =  Color.make 0. 0. 0.
+
+  let ray_trace {camera; lights; shapes} ~width ~height ~rLimit ~cLimit : Color.t list list = 
+    let get_color_at i j =
+      Camera.get_ray camera |> get_color ~i ~j
+    in
+    let rec stack_rows j acc =
+      if j >= height then acc
+      else stack_rows (j+1) (List.init width ~f:(fun i -> get_color_at i j) :: acc)
+    in
+    List.rev @@ stack_rows 0 []
 end
--- lib/scene.mli
+++ lib/scene.mli
@@ -4,10 +4,13 @@ open Camera
 open Light
 open Shape
 open Color
+
+module Intersection_record : sig
+  type t = {intersection_time: float; position: Vector3f.t; normal: Vector3f.t; }
+end
 module Scene : sig
-  type intersect_record_t = {intersection_time: float; position: Vector3f.t; normal: Vector3f.t; }
-  type global_data_t = {camera:Camera.t; lights:(module L) list; shapes:(module S) list}
-  type t
-  val get_color : Ray.t -> int -> int -> Vector3f.t
-  val ray_trace : t -> Color.t list list
+  type t = {camera:Camera.t; lights:(module L) list; shapes:(module S) list}
+  (* Gets the color at pixel (i,j), i.e. the pixel at the i-th row, j-th column *)
+  val get_color : Ray.t -> i:int -> j:int -> Color.t
+  val ray_trace : t -> width:int -> height:int -> rLimit:int -> cLimit:float -> Color.t list list
 end
