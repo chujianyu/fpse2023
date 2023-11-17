321605c1594d6385d8581f50f1952e4f9f404890
chujianyu <yu_chn@outlook.com>
2023-11-16 20:59:24 -0500

    preliminary impl of  get_ray

--- lib/camera.ml
+++ lib/camera.ml
@@ -1,15 +1,33 @@
-[@@@warning "-69-27-33"]
+[@@@warning "-69-27-33-32"]
 open Vector
 open Ray
-
+open Core
 module Camera = struct
 
-  type t = {heightAngle : float; pos : Vector3f.t; up : Vector3f.t; forward : Vector3f.t}
+  type t = {height_angle : float; pos : Vector3f.t; up : Vector3f.t; forward : Vector3f.t}
 
-  let create ~heightAngle ~pos ~up ~forward = {heightAngle; pos; up; forward}
+  let create ~height_angle ~pos ~up ~forward = {height_angle; pos; up; forward}
 
-  let getRay ~i ~j ~width ~height = 
-    let orig = Vector3f.empty () in
-    let dir = Vector3f.empty () in
-    Ray.create ~orig ~dir
+
+
+  let get_pos camera = camera.pos
+  let get_forward camera = camera.forward
+  let get_up camera = camera.up
+  let get_right camera = Vector3f.cross camera.forward camera.up
+
+  let get_ray {height_angle; pos; up; forward} ~(i:int) ~(j:int) ~(width:int) ~(height:int) = 
+    let i, j, width, height = 
+      float_of_int i, float_of_int j, float_of_int width, float_of_int height 
+    in
+    let right = Vector3f.cross forward up in
+    let ar = height /. width in
+    let width_angle = Float.atan @@ (Float.tan height_angle) /. ar in
+    let open Vector3f in
+    let right_component = right *: 2. *: Float.tan (width_angle /. 2.) in
+    let up_component = up *: 2. *: Float.tan (height_angle /. 2.) in
+    let bottom_left_pos = forward -: (right_component/:2.) -: (up_component/:2.) in
+    let pixel_pos = 
+      bottom_left_pos +: right_component *: (0.5 +.  i) /: width +: up_component *: (0.5 +.  j) /: height 
+    in
+    Ray.create ~orig:pos ~dir:pixel_pos
 end
--- lib/camera.mli
+++ lib/camera.mli
@@ -1,11 +1,16 @@
-[@@@warning "-69-27-33"]
+[@@@warning "-69-27-33-32"]
 open Vector
 open Ray
 
 module Camera : sig
   type t 
 
-  val create : heightAngle:float -> pos:Vector3f.t -> up:Vector3f.t -> forward:Vector3f.t -> t
-  (* Given indices i and j, and the width and height of the image, return the ray shot through that pixel *)
-  val getRay : i:int -> j:int -> width:int -> height:int -> Ray.t
+  val create : height_angle:float -> pos:Vector3f.t -> up:Vector3f.t -> forward:Vector3f.t -> t
+  (* Given indices i and j, and the width and height of the image, return the ray shot through that pixel 
+    where (i,j) means the pixel at the i-th row, j-th column *)
+  val get_ray : t -> i:int -> j:int -> width:int -> height:int -> Ray.t
+  val get_pos : t -> Vector3f.t
+  val get_forward : t -> Vector3f.t
+  val get_up : t -> Vector3f.t
+  val get_right : t -> Vector3f.t
 end
