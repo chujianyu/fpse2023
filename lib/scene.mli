a1638107a7c68c61830a962f52c769fdf5af2c7e
chujianyu <yu_chn@outlook.com>
2023-11-17 10:45:09 -0500

    Added comment to scene and hided helper functions

--- lib/scene.mli
+++ lib/scene.mli
@@ -6,17 +6,19 @@ open Shape
 open Color
 
 
-
-
-
+(* Scene module. Represents the 3D scene in which ray tracing can be performed *)
 module Scene : sig
 
   type t = {camera:Camera.t; lights:(module L) list; shapes:(module S) list} 
+  (* Converts the scene to a string representation *)
   val to_string : t -> string
+
+  (* Creates a scene given the necessary elements*)
   val create : camera:Camera.t -> lights:(module L) list -> shapes:(module S) list -> t
 
   (* Gets the color at pixel (i,j), i.e. the pixel at the i-th row, j-th column *)
-  val get_first_intersection : Ray.t -> (module S) list -> Intersection_record.t option
   val get_color : t -> Ray.t -> i:int -> j:int -> rLimit:int -> Color.t
+
+  (* Performs ray tracing on the entire scene *)
   val ray_trace : t -> width:int -> height:int -> rLimit:int -> cLimit:float -> Color.t list list
 end
