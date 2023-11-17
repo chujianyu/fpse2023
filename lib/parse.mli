6d1d34d542e6e3d8fd43627eb9f528be97deab48
chujianyu <yu_chn@outlook.com>
2023-11-17 10:01:18 -0500

    Added signature for parse.mli

--- lib/parse.mli
+++ lib/parse.mli
@@ -1,9 +1,10 @@
 [@@@warning "-69-27-33"]
 open Vector
+open Scene
 
-
-(* Module to parse parse the input json file as given in example_input/ *)
+(* Module to parse the input json file as given in example_input/, into Scene.t*)
 module Parse : 
   sig
-  (*define function here*)
+  (* Given the input json file name, parse the file and pack all info about the scene into Scene.t *)
+  val parse_scene : string -> Scene.t
   end
