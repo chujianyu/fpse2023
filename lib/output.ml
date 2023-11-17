8916284580068273a11a91eafd1ba35601f3df5c
chujianyu <yu_chn@outlook.com>
2023-11-17 10:30:58 -0500

    Added use of Color module

--- lib/output.ml
+++ lib/output.ml
@@ -1,30 +1,28 @@
 [@@@warning "-69-27-33-26"]
-(* Module to write an RGB file given lists of rgb tuples *)
+open Color
+open Core
+(* Module to write an RGB file given lists of lists of Color.t*)
 module Output = struct
 (*output using the ppm file format*)
-let output_rgb_data_to_file 
-(file_name:string )
-(width:int )
-(height:int)
-(rgb_list:(float*float*float) list)
+  let output_rgb_data_to_file 
+  ~(out_filename:string )
+  ~(width:int )
+  ~(height:int)
+  (colors:Color.t list list)
   =
-let output_channel = (Core.Out_channel.create file_name) in 
-let header = ["P3";
-(Int.to_string(width));
-(Int.to_string(height));"255"] in 
-
-let rgb_string_list = Core.List.fold_left
-rgb_list
-~init:[]
-~f:(fun(acc)(r,g,b)-> 
-  Int.to_string(Float.to_int(r*. 255.))::
-  Int.to_string(Float.to_int(g*. 255.))::
-  Int.to_string(Float.to_int(b*. 255.))::acc
-  )
-in 
-let add_header = (header @ Core.List.rev(rgb_string_list)) in 
-Core.Out_channel.write_all (file_name^".ppm") ~data:(Core.String.concat ~sep:"\n" add_header)
-
-
-
+  let channel_to_int channel = 
+    Float.to_int (channel *. 255.0) |> Int.max 0 |> Int.min 255 
+  in
+  let color_to_string color =
+    Printf.sprintf "%d %d %d " 
+    (channel_to_int @@ Color.get_b color) 
+    (channel_to_int @@ Color.get_g color) 
+    (channel_to_int @@ Color.get_b color) 
+  in
+  let lines = List.map colors 
+  ~f:(fun row -> String.concat ~sep:"" (List.map row ~f:color_to_string)) 
+  in
+  let content = String.concat ~sep:"\n" lines in
+  Out_channel.with_file out_filename ~f:(fun oc ->
+    Printf.fprintf oc "P3\n%d %d\n255\n%s" width height content)
 end
--- lib/output.mli
+++ lib/output.mli
@@ -1,8 +1,9 @@
 [@@@warning "-69-27-33"]
-
+open Color
+(* Module to write an RGB file given lists of lists of Color.t *)
 module Output : 
   sig
-	(*define function here*)
-  val output_rgb_data_to_file: string -> int -> int -> (float*float*float) list -> unit
+  (* writes an .ppm file given the out_filename and 2d list of colors*)
+  val output_rgb_data_to_file: out_filename:string -> Color.t list list -> unit
   end
-(* Module to write an RGB file given lists of rgb tuples *)
+
