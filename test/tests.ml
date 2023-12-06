d949e1014a65c900786a01b59852982507a2cffa
intprogacc <amitrcollege@gmail.com>
2023-12-06 14:01:29 -0500

    tests.ml change

---
+++ test/tests.ml
@@ -0,0 +1,42 @@
+open Core
+open OUnit2
+open Fpse2023_raytracer_lib
+
+
+
+
+
+
+
+
+
+
+
+let add_vector _ = 
+  let one = Fpse2023_raytracer_lib.Vector.Vector3f.create ~x:0. ~y:1. ~z:0. in 
+  let two = Fpse2023_raytracer_lib.Vector.Vector3f.create ~x:1. ~y:0. ~z:1. in 
+  let expected = Fpse2023_raytracer_lib.Vector.Vector3f.create ~x:1. ~y:1. ~z:1. in
+  let result = Vector.Vector3f.add one two in 
+  assert_equal result @@  expected
+
+
+let vector_tests =
+  "vector_tests"
+  >: test_list
+        [
+      
+  
+
+          "add_vector">:: add_vector;
+        
+          
+        
+        ]
+let series =
+  "raytracer tests"
+  >::: [
+    vector_tests;
+  ]
+
+
+  let () = run_test_tt_main series
