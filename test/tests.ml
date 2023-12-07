9329cad36f87b7596b9a593a6114c40904b37b52
intprogacc <amitrcollege@gmail.com>
2023-12-07 18:21:14 -0500

    vector tests

--- test/tests.ml
+++ test/tests.ml
@@ -20,6 +20,17 @@ let add_vector _ =
   assert_equal result @@  expected
 
 
+
+  let reflect_vector _ = 
+    let one = Fpse2023_raytracer_lib.Vector.Vector3f.create ~x:1. ~y:0. ~z:0. in 
+    let two = Fpse2023_raytracer_lib.Vector.Vector3f.create ~x:0. ~y:1. ~z:0. in 
+    
+    let expected = Fpse2023_raytracer_lib.Vector.Vector3f.create ~x:(-1.0) ~y:0. ~z:0. in
+    let result = Vector.Vector3f.reflect one two in 
+    let s = (Core.Sexp.to_string(Vector.Vector3f.sexp_of_t result))  in 
+    let () = Core.Printf.printf "%s" s in
+    assert_equal result @@  expected
+
 let vector_tests =
   "vector_tests"
   >: test_list
@@ -28,6 +39,7 @@ let vector_tests =
   
 
           "add_vector">:: add_vector;
+          "reflect_vector">:: reflect_vector;
         
           
         
